b0bf21669b4e560471ca39e0ffa3dbc0df04a12e
Hemalata <sigalapallihemalatha@gmail.com>
2026-03-18 16:40:16 +0530

    Fixed login.spec.ts for CI/CD test rerun

--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -1,10 +1,9 @@
 // login.spec.ts
 
 // Load .env only when running locally
-if (!process.env.CI) {  // CI is automatically true in GitHub Actions
+if (!process.env.CI) {
   require('dotenv').config();
 }
-
 import { test, expect } from '../../src/fixtures/baseFixture';
 import { ENV } from '../../src/config/env';
 import { testData } from '../../src/utils/testData';
@@ -24,10 +23,10 @@ console.log("URL:", BASE_URL, "USER:", USERNAME); // Debug line
 
 test('Login Test', async ({ page, loginPage }) => {
 
-  await page.goto(ENV.BASE_URL);
+  await page.goto(BASE_URL);
 
   // Use testData instead of hardcoding
-  await loginPage.login(testData.username, testData.password);
+  await loginPage.login(USERNAME, PASSWORD);
 
-  await expect(page).toHaveURL(/inventor/);
+  await expect(page).toHaveURL(/inventory/);
 });
