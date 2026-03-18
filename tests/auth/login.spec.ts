58bb0ace56867b7434666ed1beb322a6591bf4fe
Hemalata <sigalapallihemalatha@gmail.com>
2026-03-18 16:16:46 +0530

    Fixed login.spec.ts for CI/CD test rerun

--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -1,3 +1,19 @@
+
+// Load local .env only when running locally
+if (process.env.NODE_ENV !== "ci") {
+  require('dotenv').config();
+}
+
+// Access credentials from environment variables
+const URL = process.env.URL;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+
+console.log("URL:", URL, "USER:", USERNAME, "PASS:", PASSWORD); // Debug for CI/CD
+
+
+
+
 import { test, expect } from '../../src/fixtures/baseFixture';
 import { ENV } from '../../src/config/env';
 import { testData } from '../../src/utils/testData';
